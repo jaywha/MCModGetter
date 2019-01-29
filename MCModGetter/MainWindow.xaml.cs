38442b068b237951b81a7618014e4df5cd95fd25
Jay Whaley <jayw685@gmail.com>
2019-01-29 15:44:20 -0600

    Drag 'n Drop complete for mod management.
    Need to be able to delete, rename, and have optional buttons for adding or updating mods. These updates will be driven from polling the server.

--- MCModGetter/MainWindow.xaml.cs
+++ MCModGetter/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,17 +22,83 @@ namespace MCModGetter
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propName = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+        private FileSystemWatcher fileSystemWatcher;
+
+        private string _modFileLocation = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\.minecraft\mods\";
+        public string ModFileLocation { get => _modFileLocation;
+            set {
+                _modFileLocation = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            fileSystemWatcher = new FileSystemWatcher(ModFileLocation, "*.*") {
+                EnableRaisingEvents = true,
+                IncludeSubdirectories = true
+            };
+
+            fileSystemWatcher.Created += FileSystemWatcher_FileEvent;
+            fileSystemWatcher.Changed += FileSystemWatcher_FileEvent;
+            fileSystemWatcher.Renamed += FileSystemWatcher_FileEvent;
+            fileSystemWatcher.Deleted += FileSystemWatcher_FileEvent;
+
+            Directory.EnumerateFiles(ModFileLocation).Select((s)=>tvMods.Items.Add(s.Replace(ModFileLocation, "")));
         }
 
-        private void ExpanderMenuItem_ExpanderItemClick(object sender, EventArgs e)
+        #region FileSystemWatcher & TreeView Events
+        private void FileSystemWatcher_FileEvent(object sender, FileSystemEventArgs e)
         {
-            if(sender.GetType() == typeof(Label) || sender.GetType() == typeof(Image)) return;
-            else MessageBox.Show($"This menu item works and is labelled as {(sender as ExpanderMenuItem).Label}", "Test Success!!!", MessageBoxButton.OK, MessageBoxImage.Information);
+            tvMods.Dispatcher.Invoke(() =>
+            Directory.EnumerateFiles(ModFileLocation).Select((s) => tvMods.Items.Add(s.Replace(ModFileLocation, ""))), System.Windows.Threading.DispatcherPriority.Background);
         }
+
+        private void tvMods_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[]) e.Data.GetData(DataFormats.FileDrop);
+                foreach (var f in files)
+                {
+                    var newestIndex = tvMods.Items.Add(f.LastIndexOf('\\')+1);
+                    File.Copy(f, ModFileLocation+f.Substring(f.LastIndexOf('\\')+1));
+                }
+            }
+        }
+
+        private void tvMods_DragEnter(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop) || sender == e.Source)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        #endregion
+
+        #region Expander Menu Item Clicks
+        private void ExpanderMenuItem_LoginClick(object sender, EventArgs e)
+        {
+            //TODO: Login
+        }
+
+        private void ExpanderMenuItem_SettingsClick(object sender, EventArgs e)
+        {
+            //TODO: Open Settings Pane
+        }
+        #endregion
+
+        private void Label_MouseDoubleClick(object sender, MouseButtonEventArgs e) => Process.Start(ModFileLocation);
+
+        #region Utilitiy Methods
+        #endregion
     }
 }
